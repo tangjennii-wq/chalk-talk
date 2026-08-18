c2cb71a60ee8059e494c2fb58760529bc89306a0
Jenni Tang <tangjennii@gmail.com>
2026-08-17 20:45:36 -0400

    chore: stop tracking local scratch files

--- _to_delete/p5-1786990420/w.cjs
+++
@@ -1,9 +0,0 @@
-const fs=require("fs"); const p="worker.js"; let s=fs.readFileSync(p,"utf8");
-const must=(c,m)=>{ if(!c) throw new Error("ANCHOR: "+m); };
-const rep=(from,to,msg)=>{ must(s.includes(from),msg); must(s.split(from).length===2,"not unique: "+msg); s=s.replace(from,to); };
-const handler = fs.readFileSync(".p5/handler.txt","utf8");
-
-const routeFrom = '    if (request.method === "POST" && url.pathname === "/retrieve") {\n      return handleRetrieve(request, env, origin);\n    }';
-rep(routeFrom, routeFrom + '\n\n    if (request.method === "POST" && url.pathname === "/trials") {\n      return handleTrials(request, env, origin);\n    }', "route");
-rep("async function handleRetrieve(request, env, origin) {", handler + "async function handleRetrieve(request, env, origin) {", "handler");
-fs.writeFileSync(p,s); console.log("worker /trials added");
--- _to_delete/patchtmp-2026-08-09/p4.cjs
+++
@@ -1,6 +0,0 @@
-const fs=require("fs"); const p="index.html"; let s=fs.readFileSync(p,"utf8");
-const must=(c,m)=>{ if(!c) throw new Error("ANCHOR: "+m); };
-const rep=(from,to,msg)=>{ must(s.includes(from),msg); must(s.split(from).length===2,"not unique: "+msg); s=s.replace(from,to); };
-rep("    if(!_libRecent.length){", "    if(!_libView.total){", "fallback guard");
-must(!/(?<![.\w$])_libRecent\b/.test(s), "_libRecent still referenced somewhere");
-fs.writeFileSync(p,s); console.log("part 4 applied");
